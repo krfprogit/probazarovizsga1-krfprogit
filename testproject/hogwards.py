490f5dd87ada02f2e693f52a96c209684a16e6ac
krfprogit <krfprogit@gmail.com>
2021-08-17 19:54:17 +0200

    bevitt adatok formátuma egyezik a kimenettel, dátum bukik

--- testproject/hogwards.py
+++ testproject/hogwards.py
@@ -16,22 +16,37 @@ def input_kitoltes(input, adat):
 
 # bevitt adatok értékei
 user_name = "KISS RÓBERT FERENC"
-travel_date = '0020192504'
-travel_time = '0715'
+travel_date = '2019-02-04'
+travel_time = '07:15AM'
 
-# input mezők
-passenger = browser.find_element_by_id('passenger')
-departure_date = browser.find_element_by_id('departure-date')
-departure_time = browser.find_element_by_id('departure-time')
-issue_ticket = browser.find_element_by_id('issue-ticket')
+# bevitt mezők
+passenger_input = browser.find_element_by_id('passenger')
+departure_date_input = browser.find_element_by_id('departure-date')
+departure_time_input = browser.find_element_by_id('departure-time')
+issue_ticket_input = browser.find_element_by_id('issue-ticket')
 
 # bevitt adatok kitöltése
-input_kitoltes(passenger, user_name)
-input_kitoltes(departure_date, travel_date)
-input_kitoltes(departure_time, travel_time)
+input_kitoltes(passenger_input, user_name)
+input_kitoltes(departure_date_input, travel_date)
+input_kitoltes(departure_time_input, travel_time)
 time.sleep(2)
 
-issue_ticket.click()
+issue_ticket_input.click()
+
+#kitöltött adatok
+passenger_output = browser.find_element_by_id('passenger-name')
+departure_date_output1 = browser.find_element_by_id('departure-date-text')
+departure_date_output2 = browser.find_element_by_id('side-detparture-date')
+departure_time_output1 = browser.find_element_by_id('departure-time-text')
+departure_time_output2 = browser.find_element_by_id('side-departure-time')
+
+print(passenger_output.text)
+print(departure_date_output1.text)
+print(departure_date_output2.text)
+print(departure_time_output1.text)
+print(departure_time_output2.text)
+
+
 
 
 # time.sleep(2)
