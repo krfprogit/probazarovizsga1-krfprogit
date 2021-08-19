57f8a6dfa64f061eda0271986e8d1fd858744632
krfprogit <krfprogit@gmail.com>
2021-08-19 19:49:07 +0200

    oldal betölt

---
+++ testproject/lottery.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+import csv
+import datetime #Py10
+
+from selenium.webdriver.common.keys import Keys
+import selenium.common.exceptions
+
+PATH = "C:\\Windows\\chromedriver.exe"
+browser = webdriver.Chrome(PATH)
+
+URL = "https://witty-hill-0acfceb03.azurestaticapps.net/lottery.html"
+browser.get(URL)
+browser.maximize_window()
+
+time.sleep(2)
+browser.quit()
