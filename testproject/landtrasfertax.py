1359539011b21c4fde97b279f0bb4c63f5184f36
krfprogit <krfprogit@gmail.com>
2021-08-19 21:47:17 +0200

    TC01 kész

--- testproject/landtrasfertax.py
+++ testproject/landtrasfertax.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 import csv
-import datetime #Py10
+import datetime  # Py10
 
 from selenium.webdriver.common.keys import Keys
 import selenium.common.exceptions
@@ -13,5 +13,20 @@ URL = "https://witty-hill-0acfceb03.azurestaticapps.net/landtransfertax.html"
 browser.get(URL)
 browser.maximize_window()
 
-time.sleep(2)
-browser.quit()
+# input értékek
+price_input = browser.find_element_by_id('price')
+tax_output = browser.find_element_by_id('tax')
+go_btn = browser.find_element_by_xpath('/html/body/main/div/div/p[1]/button')
+
+# TC01
+# "Land Transfer Fee" feliratú mező pontosan üres marad-e?
+go_btn.click()
+time.sleep(1)
+assert tax_output.text == ''
+
+# megjelenik-e a következő felirat: "Enter the property value before clicking Go button."?
+disclaimer_output = browser.find_element_by_id('disclaimer')
+assert disclaimer_output.is_displayed()
+
+# time.sleep(2)
+# browser.quit()
