94e002e5d53d9b4903196f36e1df4d012d65ffe3
krfprogit <krfprogit@gmail.com>
2021-08-19 21:34:25 +0200

    kész DEF-el

--- testproject/lottery.py
+++ testproject/lottery.py
@@ -13,11 +13,17 @@ time.sleep(1)
 generate_btn = browser.find_element_by_id('draw-number')
 reset_btn = browser.find_element_by_id('reset-numbers')
 
+
+def find_numbers():
+    numbers_drawn = browser.find_elements_by_xpath('//*[@id="container"]/div')
+    numbers_quantity = len(numbers_drawn)
+    return numbers_quantity
+
+
 # TC01
 # ures lista
-numbers_drawn = browser.find_elements_by_xpath('//*[@id="container"]/div')
-numbers_quantity = len(numbers_drawn)
-assert numbers_quantity == 0
+
+assert find_numbers() == 0
 
 # TC02
 # 6x click
@@ -26,9 +32,8 @@ for i in range(6):
     time.sleep(1)
 
 # számok darabszáma = 6?
-numbers_drawn = browser.find_elements_by_xpath('//*[@id="container"]/div')
-numbers_quantity = len(numbers_drawn)
-assert numbers_quantity == 6
+
+assert find_numbers() == 6
 
 # számok 1 és 59 között vannak?
 # for i in range(1, 7):
@@ -38,6 +43,7 @@ assert numbers_quantity == 6
 # print('---------------------')
 
 # számok 1 és 59 között vannak?
+numbers_drawn = browser.find_elements_by_xpath('//*[@id="container"]/div')
 for n in numbers_drawn:
     numbers_value = n.text
     print(numbers_value, ' ', end='')
@@ -48,16 +54,14 @@ for n in numbers_drawn:
 # hetedik click, csak 6 szám
 generate_btn.click()
 time.sleep(1)
-numbers_drawn = browser.find_elements_by_xpath('//*[@id="container"]/div')
-numbers_quantity = len(numbers_drawn)
-assert numbers_quantity != 7
+
+assert find_numbers() != 7
 
 # reset, nincs szám
 reset_btn.click()
 time.sleep(1)
-numbers_drawn = browser.find_elements_by_xpath('//*[@id="container"]/div')
-numbers_quantity = len(numbers_drawn)
-assert numbers_quantity == 0
+
+assert find_numbers() == 0
 
 time.sleep(2)
 browser.quit()
